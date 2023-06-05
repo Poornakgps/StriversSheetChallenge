71a6724ba1d5d4e3a38f04fd093db20d0535a024
Poornachandra123 <96694008+Poornachandra123@users.noreply.github.com>
2023-06-06 01:09:32 +0530

    sort012 done

---
+++ 0_1_2_sort/soln.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   int l=0, r=n-1, mid=0;
+   while(mid<=r){
+
+      if(arr[mid]==0){
+         swap(arr[l],arr[mid]);
+         l++,mid++;
+         continue;
+      }
+      else if(arr[mid]==2){
+         swap(arr[r], arr[mid]);
+         r--;
+         continue;
+      }
+      mid++;  
+   }
+}
+
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+        sort012(&arr,n);
+        for(auto it: arr){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+    }
+}
